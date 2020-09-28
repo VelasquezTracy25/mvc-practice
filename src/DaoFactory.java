8b08967ab209e458e43c8de2751c8c12af4dcfb6
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-09-27 22:15:13 -0500

    cleaned up daofactory

--- src/DaoFactory.java
+++ src/DaoFactory.java
@@ -1,2 +1,33 @@
 public class DaoFactory {
+
+    //reference the interface to access the dao
+    private static Plants plantsDao;
+    private static Fertilizers fertilizersDao;
+    private static Pesticides pesticidesDao;
+
+//any bean type we have will get a method called get[Bean]Dao
+
+    // will run the PlantsDao method to create new plants if it is empty
+    public static Plants getPlantsDao() {
+        if (plantsDao == null) {
+            plantsDao = new PlantsDao();
+        }
+        return plantsDao;
+    }
+
+    public static Fertilizers getFertilizersDao() {
+        if (fertilizersDao == null) {
+            fertilizersDao = new FertilizersDao();
+        }
+        return fertilizersDao;
+    }
+
+    public static Pesticides getPesticidesDao() {
+        if (pesticidesDao == null) {
+            pesticidesDao = new PesticidesDao();
+        }
+        return pesticidesDao;
+    }
+
+
 }
--- src/PlantsDao.java
+++ src/PlantsDao.java
@@ -1,2 +1,23 @@
-public class PlantsDao {
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlantsDao implements Plants{
+
+//create temporary list to hold plants as they are created
+    private List<Plant> plants = new ArrayList<>();
+
+    public PlantsDao(){
+
+    }
+
+
+    @Override
+    public Plant findByID(long id) {
+        return null;
+    }
+
+    @Override
+    public long createPlant(Plant plant) {
+        return 0;
+    }
 }
